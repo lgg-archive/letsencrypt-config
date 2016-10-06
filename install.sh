9054e090116e0ee0068caeee21cc21f1285913cc
littleguga <fed777os@gmail.com>
2016-10-06 07:22:27 +0300

    add aliases bu default

--- install.sh
+++ install.sh
@@ -15,6 +15,12 @@ chmod +x /etc/letssl/run_letsencrypt && chmod +x /etc/letssl/update_ssl
 mkdir -p /usr/local/letssl/
 cp ./usr/local/letssl/config-sample.ini /usr/local/letssl/sample
 
+# add aliases for help and lists
+echo "
+alias ls_l='ls -l /etc/letsencrypt/live'
+alias ls_h='cat /etc/letssl/help'
+alias ls_lc='ls -l /usr/local/letssl/'" >> ~/.bash_aliases
+
 # check if we have no errors
 if [ $? -eq 0 ]; then
     echo "Setup completed. Add cron task, edit nginx let's encrypt configs"
